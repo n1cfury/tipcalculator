e69b40701f34388393045da00420d7070b46b5f6
n1cfury <fukugetin@gmail.com>
2017-02-01 13:19:16 -0800

    Added functions for the tip calculator and prompted for an amount

--- iCalc.py
+++ iCalc.py
@@ -12,14 +12,20 @@ total = meal + meal * tip
 
 #The command below designates how many digits passed the decimal I want displayed. Here, I want to tenths place only (rounded up)
 print("%.1f" % total)  '''
+def banner():
+	print "###### Tip Calculator #########"
+	print "Enter the amount when prompted"
+	print ""
+	
+def main():
+	banner()
+	meal = float(raw_input("How much was your meal? "))			#Asks for cost of meal
+	tax = meal * 0.075
+	tip = 0.15
+	mealtotal = meal + tax
+	finaltotal = total + (tip * mealtotal)							#Calculated final total
+	print ("Your meal was %s with an added %s for tax.") % (meal, tax)
+	print ("Including the tip the meal is %s") % round(finaltotal, 2)
 
-
-meal = float(raw_input("How much was your meal? "))			#Asks for cost of meal
-tax = meal * 0.075
-tip = 0.15
-total = meal + tax
-gratuiity = total + (tip * total)							#Calculated final total
-
-
-print ("Your meal was %s with an added %s for tax.") % (meal, tax)
-print ("Including the tip the meal is %s") % round(gratuiity, 2)
+if __name__ == "__main__":
+  main()
